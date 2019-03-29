b4919464864896a9485e535c0de9932c75e96440
Boruch Z <philomath613@gmail.com>
2019-03-29 19:28:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,18 +14,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z].+\W$/) ? true : false
 end
 
-def valid_phone_number?(phone)
-  phone.scan(/\b\(?\d{3}[\s\W)]?\d{3}[\s\W]?\d{4}\b/) ? true : false
-end
-
-# valid_numbers = ["24388945461", "(718)891-1313", "234 435 9978", "(800)4261134"]
-#
-# p valid_numbers.all? do |i|
-#   valid_phone_number?(i)
-# end
 
 phone_regex = %r{
-  \(?     # first optional paren
+  \(?       # first optional paren
   \d{3}     # area code
   [\s\W)]?  # second optional paren or space
   \d{3}     # exchange
@@ -34,4 +25,16 @@ phone_regex = %r{
   \b
 }x
 
-p "2438894546 (718)891-1313 234 435 9978 (800)4261134".scan(phone_regex)
+def valid_phone_number?(phone)
+  phone.scan(phone_regex) ? true : false
+end
+
+# valid_numbers = ["24388945461", "(718)891-1313", "234 435 9978", "(800)4261134"]
+#
+# p valid_numbers.all? do |i|
+#   valid_phone_number?(i)
+# end
+
+
+
+# p "2438894546 (718)891-1313 234 435 9978 (800)4261134".scan(phone_regex)
